4d5d0f4e38d65a09b14ebe889ffba9efa9eddc9c
Caylin Dewey <caylindewey42@gmail.com>
2024-01-16 09:56:06 +0000

    Update filepath company.json in run.py

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def index():
 def about():
     data = []
     with open(
-        "/workspace/thorin-flask-project/.devcontainer/data/company.json", "r"
+        "/workspace/thorin-flask-project/data/company.json", "r"
     ) as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
@@ -27,7 +27,7 @@ def about():
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
-    with open("/workspace/thorin-flask-project/.devcontainer/data/company.json", "r") as json_data:
+    with open("/workspace/thorin-flask-project/data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
